2e99593300369cc14033b5a7d9512949cbf07efd
Vishal Chaudhary <vishalchaudhary5794@gmail.com>
2023-07-09 00:31:11 +0530

    Finally Completed

--- README.md
+++ README.md
@@ -8,5 +8,5 @@
 2. Spaceship shoots down those asteroids thus increasing the score level.
 3. Control the Left and right movement of the Spaceship using Arrow Key Left and Right respectively.
 4. To shoot bullets keep pressing SPACE
-5. Game window closes as soon as any asteroid hits the spaceship indicating Game Over.
+5. Game window closes as soon as any asteroid hits the spaceship indicating Game Over and displaying the score made.
 ### Enjoy the game....
--- main.py
+++ main.py
@@ -8,13 +8,11 @@ pygame.display.set_caption("Space Invaders")
 pygame.display.set_icon(pygame.image.load("images/icon.png"))
 
 background = pygame.image.load("images/background.png")
+score_screen_bg = pygame.image.load('images/score_screen_bg.png')
 
 pygame.mixer.music.load("music/background_music.mp3")
 pygame.mixer.music.play(-1)
 pygame.mixer.music.set_volume(0.35)
-
-sound_effect = pygame.mixer.Sound('music/asteroid_explosion.mp3')
-sound_effect.set_volume(0.175)
 
 
 class Spaceship(pygame.sprite.Sprite):
@@ -48,8 +46,8 @@ class Asteroid(pygame.sprite.Sprite):
         super().__init__()
         self.x_pos = x_pos
         self.y_pos = y_pos
-        self.speed_x = random.randint(6, 8)
-        self.speed_y = random.uniform(0.9, 1.5)
+        self.speed_x = random.choice([-7, -6, -4, 4, 6, 7])
+        self.speed_y = random.uniform(0.9, 2)
         self.image = pygame.image.load(image_path)
         self.rect = pygame.Rect(self.x_pos, self.y_pos, 64, 64)
 
@@ -107,19 +105,24 @@ class Bullet(pygame.sprite.Sprite):
 
 spaceship = Spaceship(418, 536, "images/spaceship.png")
 
-asteroid_1 = Asteroid(random.randint(0, 836), random.randint(0, 86), "images/asteroid.png")
-asteroid_2 = Asteroid(random.randint(0, 836), random.randint(0, 86), "images/asteroid2.png")
-asteroid_3 = Asteroid(random.randint(0, 836), random.randint(0, 86), "images/asteroid3.png")
-asteroid_4 = Asteroid(random.randint(0, 836), random.randint(0, 86), "images/asteroids4.png")
+asteroid_1 = Asteroid(random.randint(0, 836), random.randint(0, 64), "images/asteroid.png")
+asteroid_2 = Asteroid(random.randint(0, 836), random.randint(0, 64), "images/asteroid2.png")
+asteroid_3 = Asteroid(random.randint(0, 836), random.randint(0, 64), "images/asteroid3.png")
+asteroid_4 = Asteroid(random.randint(0, 836), random.randint(0, 64), "images/asteroids4.png")
+asteroid_5 = Asteroid(random.randint(0, 836), random.randint(0, 64), "images/asteroid5.png")
+asteroid_6 = Asteroid(random.randint(0, 836), random.randint(0, 64), "images/asteroid6.png")
+asteroid_7 = Asteroid(random.randint(0, 836), random.randint(0, 64), "images/asteroid7.png")
 
 bullet = Bullet(418, 536, "images/bullet.png")
 shoot = False
 
 asteroids = pygame.sprite.Group()
-asteroids.add(asteroid_1, asteroid_2, asteroid_3, asteroid_4)
+asteroids.add(asteroid_1, asteroid_2, asteroid_3, asteroid_4, asteroid_5, asteroid_6, asteroid_7)
 score_counter = 0
 font = pygame.font.SysFont('calibri', 20, bold=False, italic=False)
 clock = pygame.time.Clock()
+score_screen = False
+
 
 # Game loop
 running = True
@@ -134,10 +137,10 @@ while running:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                spaceship.speed = -7
+                spaceship.speed = -10
 
             elif event.key == pygame.K_RIGHT:
-                spaceship.speed = 7
+                spaceship.speed = 10
 
             if event.key == pygame.K_SPACE:
                 shoot = True
@@ -149,40 +152,46 @@ while running:
             if event.key == pygame.K_SPACE:
                 shoot = False
 
-    # Designing the game logic
-    if shoot:
-        bullet.speed_y = -30
-    spaceship.update()
-    asteroids.update()
-    bullet.update()
+    if not score_screen:
+        # Designing the game logic
+        if shoot:
+            bullet.speed_y = -34
+        spaceship.update()
+        asteroids.update()
+        bullet.update()
 
-    asteroids_and_bullet = pygame.sprite.spritecollide(bullet, asteroids, dokill=True)
+        asteroids_and_bullet = pygame.sprite.spritecollide(bullet, asteroids, dokill=True)
 
-    for i in asteroids_and_bullet:
-        score_counter += 1
-        sound_effect.play()
+        for i in asteroids_and_bullet:
+            score_counter += 1
+            i.x_pos = random.randint(0, 836)
+            i.y_pos = random.randint(0, 64)
+            i.speed_x = random.choice([-7, -6, -4, 4, 6, 7])
+            i.speed_y = random.uniform(0.9, 2)
+            asteroids.add(i)
 
-    asteroids_and_spaceship_collide = pygame.sprite.spritecollide(spaceship, asteroids, dokill=False)
+        asteroids_and_spaceship_collide = pygame.sprite.spritecollide(spaceship, asteroids, dokill=False)
 
-    for i in asteroids_and_spaceship_collide:
-        if pygame.sprite.collide_rect(spaceship, i):
-            pygame.mixer.music.stop()
-            running = False
+        for i in asteroids_and_spaceship_collide:
+            if pygame.sprite.collide_rect(spaceship, i):
+                score_screen = True
 
-    if len(asteroids.sprites()) == 0:
-        asteroid_1.x_pos, asteroid_1.y_pos = random.randint(0, 836), random.randint(0, 86)
-        asteroid_2.x_pos, asteroid_2.y_pos = random.randint(0, 836), random.randint(0, 86)
-        asteroid_3.x_pos, asteroid_3.y_pos = random.randint(0, 836), random.randint(0, 86)
-        asteroid_4.x_pos, asteroid_4.y_pos = random.randint(0, 836), random.randint(0, 86)
-        asteroids.add(asteroid_1, asteroid_2, asteroid_3, asteroid_4)
+        # Rendering the game graphics
+        game_win.blit(background, (0, 0))
+        bullet.draw(game_win)
+        spaceship.draw(game_win)
+        asteroids.draw(game_win)
+        text = font.render(f"Score - {score_counter}", True, (200, 100, 150), None)
+        game_win.blit(text, (816, 20))
 
-    # Rendering the game graphics
-    game_win.blit(background, (0, 0))
-    bullet.draw(game_win)
-    spaceship.draw(game_win)
-    asteroids.draw(game_win)
-    text = font.render(f"Score - {score_counter}", True, (200, 100, 150), None)
-    game_win.blit(text, (816, 20))
+    if score_screen:
+        pygame.mixer.music.stop()
+        game_win.blit(score_screen_bg, (0, 0))
+        font2 = pygame.font.SysFont('calibri', 30)
+        game_over = font2.render('GAME OVER', True, (108, 137, 154))
+        score = font2.render(f'SCORE - {score_counter}', True, (108, 137, 154))
+        game_win.blit(game_over, (370, 270))
+        game_win.blit(score, (370, 310))
 
     # Updating the game window
     pygame.display.flip()
